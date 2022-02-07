890b510df4ff726660432331838a7efaf04b9890
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-07 09:43:05 -0800

    add index.js file and api call
    Co-Authored By: Eric Crudup <briefcasemanx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import Template from "./js/template.js";
 
 $(document).ready(function () {
+  $("#weatherLocation").click(function () {
+    const city = $("#location").val();
+    $("#location").val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      const kelvinT = response.main.temp;
+      const fahrT = Math.round((((kelvinT - 273.15) * 9) / 5 + 32) * 100) / 100;
+      $(".showHumidity").text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $(".showTemp").text(`The temperature in Kelvins is ${fahrT} degrees.`);
+    }
+  });
 });
